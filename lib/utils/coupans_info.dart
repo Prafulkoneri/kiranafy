3914a71bd31cf1505ab4b403a08324e28a0d99e2
Chetan Rathod <chetan.r@mplussoft.in>
2023-04-28 17:22:01 +0530

    nirdesh

--- lib/utils/coupans_info.dart
+++ lib/utils/coupans_info.dart
@@ -4,7 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:local_supper_market/const/color.dart';
-Widget CouponsBottomSheet() {
+Widget CouponsBottomSheet(context) {
   return Stack(
     clipBehavior: Clip.none,
     children: [
@@ -17,9 +17,12 @@ Widget CouponsBottomSheet() {
         width: 390.w,
         decoration: new BoxDecoration(
           color: Colors.white,
-          borderRadius: new BorderRadius.only(
-            topLeft: const Radius.circular(25.0),
-            topRight: const Radius.circular(25.0),
+          borderRadius:
+          new BorderRadius.only(
+            topLeft:
+            const Radius.circular(25.0),
+            topRight:
+            const Radius.circular(25.0),
           ),
         ),
         child: Column(
@@ -27,26 +30,38 @@ Widget CouponsBottomSheet() {
             Row(
               children: [
                 Padding(
-                  padding: EdgeInsets.only(left: 13.w, top: 20.w),
-                  child: Image.asset('assets/images/couponslogo.png',
-                      height: 33.h, width: 21.w),
+                  padding: EdgeInsets.only(
+                      left: 13.w,
+                      top: 20.w),
+                  child: Image.asset(
+                      'assets/images/couponslogo.png',
+                      height: 33.h,
+                      width: 21.w),
                 ),
                 SizedBox(
                   width: 10.w,
                 ),
                 Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment:
+                  CrossAxisAlignment
+                      .start,
                   children: [
                     Padding(
-                      padding: EdgeInsets.only(top: 18.0.h),
+                      padding:
+                      EdgeInsets.only(
+                          top: 18.0.h),
                       child: Text(
                         "Hi Choice Supermarket",
-                        style: GoogleFonts.inter(
+                        style: GoogleFonts
+                            .inter(
                           textStyle: const TextStyle(
                               color: Black,
-                              letterSpacing: .5,
+                              letterSpacing:
+                              .5,
                               fontSize: 18,
-                              fontWeight: FontWeight.w700),
+                              fontWeight:
+                              FontWeight
+                                  .w700),
                         ),
                       ),
                     ),
@@ -55,12 +70,16 @@ Widget CouponsBottomSheet() {
                     ),
                     Text(
                       "Valid until 16 March 2023",
-                      style: GoogleFonts.inter(
+                      style:
+                      GoogleFonts.inter(
                         textStyle: const TextStyle(
                             color: Black,
-                            letterSpacing: .5,
+                            letterSpacing:
+                            .5,
                             fontSize: 12,
-                            fontWeight: FontWeight.w400),
+                            fontWeight:
+                            FontWeight
+                                .w400),
                       ),
                     ),
                   ],
@@ -68,83 +87,75 @@ Widget CouponsBottomSheet() {
               ],
             ),
             Padding(
-              padding: EdgeInsets.only(left: 45.w),
+              padding: EdgeInsets.only(
+                  left: 45.w),
               child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment:
+                CrossAxisAlignment.end,
                 // mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("50% OFF",
-                      style: GoogleFonts.dmSans(
-                        textStyle: TextStyle(
-                            color: Black,
-                            letterSpacing: .5,
-                            fontSize: 16.sp,
-                            fontWeight: FontWeight.w700),
-                      )),
-                  SizedBox(
-                    width: 10.w,
+                  Row(
+                    children: [
+                      Text("50% OFF",
+                          style: GoogleFonts
+                              .dmSans(
+                            textStyle: TextStyle(
+                                color: Black,
+                                letterSpacing:
+                                .5,
+                                fontSize: 16.sp,
+                                fontWeight:
+                                FontWeight
+                                    .w700),
+                          )),
+                      SizedBox(
+                        width: 10.w,
+                      ),
+                      Text("UPTO ₹120",
+                          style: GoogleFonts
+                              .dmSans(
+                            textStyle: TextStyle(
+                                color: Grey,
+                                letterSpacing:
+                                .5,
+                                fontSize: 12.sp,
+                                fontWeight:
+                                FontWeight
+                                    .w400),
+                          )),
+                    ],
                   ),
-                  Text("UPTO ₹120",
-                      style: GoogleFonts.dmSans(
-                        textStyle: TextStyle(
-                            color: Grey,
-                            letterSpacing: .5,
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w400),
-                      )),
                   Padding(
-                    padding: EdgeInsets.only(left: 50.0.w, bottom: 0.w),
+                    padding:
+                    EdgeInsets.only(
+                        bottom: 0.w,right: 13.w),
                     child: Row(
                       children: [
-                        SizedBox(
-                          width: 113.w,
-                          height: 34.h,
-                          child: ElevatedButton(
-                            // style: ElevatedButton.styleFrom(
-                            //   //<-- SEE HERE
-                            //   side: BorderSide(
-                            //     width: 3.0,
-                            //   ),
-                            // ),
-                            style: ButtonStyle(
-                              elevation: MaterialStateProperty.all(0),
-
-                              // backgroundColor: ,
-                              backgroundColor:
-                              MaterialStateProperty.all(Colors.white),
-                              shape: MaterialStateProperty.all(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  side: BorderSide(width: 1, color: SplashText),
-                                ),
-                              ),
-                            ),
-                            onPressed: () {},
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 11.33.w,vertical: 7.w),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.w),
+                            border: Border.all(color: SplashText)
+                          ),
+                          child: Center(
                             child: Row(
-                              mainAxisSize: MainAxisSize.min,
                               children: [
-                                Text(
-                                  "OFF100",
-                                  style: GoogleFonts.dmSans(
-                                    textStyle: TextStyle(
-                                        color: SplashText,
-                                        letterSpacing: .5,
-                                        fontSize: 12.sp,
-                                        fontWeight: FontWeight.w700),
-                                  ),
-                                ), // <-- Text
+                                Text("OFF100",style: TextStyle(fontWeight: FontWeight.w700,fontSize: 16.sp,color: SplashText),),
                                 SizedBox(
-                                  width: 19.w,
+                                  width:
+                                  19.w,
                                 ),
-                                SvgPicture.asset(
+                                SvgPicture
+                                    .asset(
                                   'assets/images/svg2.svg',
-                                  width: 17.w,
-                                  height: 17.h,
+                                  width:
+                                  17.w,
+                                  height:
+                                  17.h,
                                 ),
                               ],
                             ),
-
-                            //
                           ),
                         ),
                       ],
@@ -166,8 +177,10 @@ Widget CouponsBottomSheet() {
             Column(
               children: [
                 Padding(
-                  padding:
-                  EdgeInsets.only(left: 17.75.w, top: 8.w, bottom: 8.w),
+                  padding: EdgeInsets.only(
+                      left: 17.75.w,
+                      top: 8.w,
+                      bottom: 8.w),
                   child: Row(
                     children: [
                       SvgPicture.asset(
@@ -180,23 +193,32 @@ Widget CouponsBottomSheet() {
                       ),
                       Text(
                         "Lorem ipsum dolor sit amet, consectetur",
-                        style: GoogleFonts.dmSans(
+                        style: GoogleFonts
+                            .dmSans(
                           textStyle: TextStyle(
                               height: 1.5,
                               color: black,
-                              letterSpacing: .5,
-                              fontSize: 12.sp,
-                              fontWeight: FontWeight.w400),
+                              letterSpacing:
+                              .5,
+                              fontSize:
+                              12.sp,
+                              fontWeight:
+                              FontWeight
+                                  .w400),
                         ),
                       )
                     ],
                   ),
                 ),
                 Padding(
-                  padding:
-                  EdgeInsets.only(left: 17.75.w, top: 8.w, right: 13.w),
+                  padding: EdgeInsets.only(
+                      left: 17.75.w,
+                      top: 8.w,
+                      right: 13.w),
                   child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                    crossAxisAlignment:
+                    CrossAxisAlignment
+                        .start,
                     children: [
                       SvgPicture.asset(
                         'assets/images/checked.svg',
@@ -210,15 +232,25 @@ Widget CouponsBottomSheet() {
                         child: Text(
                           maxLines: 2,
                           "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sit nunc, netus ac vulputate sed",
-                          textAlign: TextAlign.justify,
-                          style: GoogleFonts.dmSans(
+                          textAlign:
+                          TextAlign
+                              .justify,
+                          style: GoogleFonts
+                              .dmSans(
                             textStyle: TextStyle(
                                 height: 1.5,
-                                color: black,
-                                letterSpacing: .05,
-                                overflow: TextOverflow.ellipsis,
-                                fontSize: 12.sp,
-                                fontWeight: FontWeight.w400),
+                                color:
+                                black,
+                                letterSpacing:
+                                .05,
+                                overflow:
+                                TextOverflow
+                                    .ellipsis,
+                                fontSize:
+                                12.sp,
+                                fontWeight:
+                                FontWeight
+                                    .w400),
                           ),
                         ),
                       )
@@ -226,8 +258,10 @@ Widget CouponsBottomSheet() {
                   ),
                 ),
                 Padding(
-                  padding:
-                  EdgeInsets.only(left: 17.75.w, top: 8.w, bottom: 8.w),
+                  padding: EdgeInsets.only(
+                      left: 17.75.w,
+                      top: 8.w,
+                      bottom: 8.w),
                   child: Row(
                     children: [
                       SvgPicture.asset(
@@ -240,21 +274,28 @@ Widget CouponsBottomSheet() {
                       ),
                       Text(
                         "Lorem ipsum dolor sit amet, consectetur",
-                        style: GoogleFonts.dmSans(
+                        style: GoogleFonts
+                            .dmSans(
                           textStyle: TextStyle(
                               color: black,
                               height: 1.5,
-                              letterSpacing: .5,
-                              fontSize: 12.sp,
-                              fontWeight: FontWeight.w400),
+                              letterSpacing:
+                              .5,
+                              fontSize:
+                              12.sp,
+                              fontWeight:
+                              FontWeight
+                                  .w400),
                         ),
                       )
                     ],
                   ),
                 ),
                 Padding(
-                  padding:
-                  EdgeInsets.only(left: 17.75.w, top: 8.w, bottom: 8.w),
+                  padding: EdgeInsets.only(
+                      left: 17.75.w,
+                      top: 8.w,
+                      bottom: 8.w),
                   child: Row(
                     children: [
                       SvgPicture.asset(
@@ -267,26 +308,37 @@ Widget CouponsBottomSheet() {
                       ),
                       Text(
                         "Lorem ipsum dolor sit amet, consectetur",
-                        style: GoogleFonts.dmSans(
+                        style: GoogleFonts
+                            .dmSans(
                           textStyle: TextStyle(
                               color: black,
                               height: 1.5,
-                              letterSpacing: .5,
-                              fontSize: 12.sp,
-                              fontWeight: FontWeight.w400),
+                              letterSpacing:
+                              .5,
+                              fontSize:
+                              12.sp,
+                              fontWeight:
+                              FontWeight
+                                  .w400),
                         ),
                       )
                     ],
                   ),
                 ),
                 Padding(
-                  padding:
-                  EdgeInsets.only(left: 17.75.w, top: 8.w, right: 13.w),
+                  padding: EdgeInsets.only(
+                      left: 17.75.w,
+                      top: 8.w,
+                      right: 13.w),
                   child: Row(
                     children: [
                       Padding(
-                        padding: EdgeInsets.only(bottom: 10.w),
-                        child: SvgPicture.asset(
+                        padding:
+                        EdgeInsets.only(
+                            bottom:
+                            10.w),
+                        child: SvgPicture
+                            .asset(
                           'assets/images/checked.svg',
                           width: 17.w,
                           height: 17.h,
@@ -300,16 +352,26 @@ Widget CouponsBottomSheet() {
                           softWrap: true,
                           maxLines: 3,
                           "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sit nunc, netus ac vulputate sed",
-                          textAlign: TextAlign.justify,
-                          style: GoogleFonts.dmSans(
-                            textStyle: TextStyle(
-                                color: black,
-                                height: 1.5,
+                          textAlign:
+                          TextAlign
+                              .justify,
+                          style: GoogleFonts
+                              .dmSans(
+                            textStyle:
+                            TextStyle(
+                                color:
+                                black,
+                                height:
+                                1.5,
 
                                 // letterSpacing: .05,
-                                overflow: TextOverflow.ellipsis,
-                                fontSize: 12.sp,
-                                fontWeight: FontWeight.w400),
+                                overflow:
+                                TextOverflow
+                                    .ellipsis,
+                                fontSize: 12
+                                    .sp,
+                                fontWeight:
+                                FontWeight.w400),
                           ),
                         ),
                       )
@@ -329,6 +391,7 @@ Widget CouponsBottomSheet() {
           right: 0.w,
           child: InkWell(
             onTap: () {
+              Navigator.pop(context);
             },
             child: Container(
               height: 50.w,
