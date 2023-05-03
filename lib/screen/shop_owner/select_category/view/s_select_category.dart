d27812ed9bb834413c1dd9b367f4b987035b389a
Rushi2505 <rushikesh.p@mplussoft.in>
2023-05-03 08:58:07 +0530

    pull 3-5-23

---
+++ lib/screen/shop_owner/select_category/view/s_select_category.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ShopSelectCategory extends StatefulWidget {
+  const ShopSelectCategory({super.key});
+
+  @override
+  State<ShopSelectCategory> createState() => _ShopSelectCategoryState();
+}
+
+class _ShopSelectCategoryState extends State<ShopSelectCategory> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
