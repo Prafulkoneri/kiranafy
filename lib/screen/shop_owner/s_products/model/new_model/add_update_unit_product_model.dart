7d01aacb9b851a42858d2e9b5e64689c8cc42672
Rushi2505 <rushikesh.p@mplussoft.in>
2023-07-04 20:10:44 +0530

    29-6-2023 pull

--- lib/screen/shop_owner/s_products/model/new_model/add_update_unit_product_model.dart
+++ lib/screen/shop_owner/s_products/model/new_model/add_update_unit_product_model.dart
@@ -1,24 +1,44 @@
 class AddUpdateUnitProductCategoryRequestModel {
+  String? actionType;
+  String? productId;
+  String? unitId;
+  String? weight;
+  String? mrpPrice;
+  String? offerPrice;
+  String? status;
   String? productUnitId;
+  String? unitBasedProductImage1Path;
+  String? unitBasedProductImage2Path;
+  String? unitBasedProductImage3Path;
   String? productType;
   AddUpdateUnitProductCategoryRequestModel({
+    this.actionType,
+    this.productId,
+    this.unitId,
+    this.weight,
+    this.mrpPrice,
+    this.offerPrice,
+    this.status,
     this.productUnitId,
+    this.unitBasedProductImage1Path,
+    this.unitBasedProductImage2Path,
+    this.unitBasedProductImage3Path,
     this.productType,
   });
 
   Map<String, dynamic> toJson() {
     Map<String, dynamic> data = {};
-    data["action_type"] = productUnitId;
-    data["product_id"] = productType;
-    data["unit_id"] = productType;
-    data["weight"] = productType;
-    data["mrp_price"] = productType;
-    data["offer_price"] = productType;
-    data["status"] = productType;
-    data["product_unit_id"] = productType;
-    data["unit_based_product_image_1_path"] = productType;
-    data["unit_based_product_image_2_path"] = productType;
-    data["unit_based_product_image_3_path"] = productType;
+    data["action_type"] = actionType;
+    data["product_id"] = productId;
+    data["unit_id"] = unitId;
+    data["weight"] = weight;
+    data["mrp_price"] = mrpPrice;
+    data["offer_price"] = offerPrice;
+    data["status"] = status;
+    data["product_unit_id"] = productUnitId;
+    data["unit_based_product_image_1_path"] = unitBasedProductImage1Path;
+    data["unit_based_product_image_2_path"] = unitBasedProductImage2Path;
+    data["unit_based_product_image_3_path"] = unitBasedProductImage3Path;
     data["product_type"] = productType;
 
     return data;
