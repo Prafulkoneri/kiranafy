cd3c4dbb5f2f755556e85af6cd6f80a63f1c3afb
Mplussoft <mplussoft@Manishs-MacBook-Air.local>
2023-10-10 17:52:38 +0530

    pull

--- lib/screen/customer/home/view/home_screen_view.dart
+++ lib/screen/customer/home/view/home_screen_view.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:connectivity/connectivity.dart';
+// import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
