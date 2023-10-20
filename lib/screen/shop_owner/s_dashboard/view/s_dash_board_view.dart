780f10efe23a45b7a15607aa844c0779e1189ad0
praful <prafullkoneri@gmail.com>
2023-10-20 18:29:49 +0530

    push

--- lib/screen/shop_owner/s_dashboard/view/s_dash_board_view.dart
+++ lib/screen/shop_owner/s_dashboard/view/s_dash_board_view.dart
@@ -95,23 +95,25 @@ class _ShopDashBoardViewState extends State<ShopDashBoardView> {
                             ),
                           ),
                           GestureDetector(
-                            onTap: () {
-                              readMain.onNavigation(
-                                  0,
-                                  NotificationsScreenView(route: "dashboard"),
-                                  context);
-                              readMain.hideBottomNavigationBar();
-                            },
-                            child: watch.notificationdata?.notificationStatus ==
-                                    "unseen"
-                                ? SvgPicture.asset(
-                                    "assets/icons/notification_shop.svg")
-                                : Container(
-                                    height: 50.h,
-                                    width: 50.h,
-                                    color: Black,
-                                  ),
-                          )
+                              onTap: () {
+                                readMain.onNavigation(
+                                    0,
+                                    NotificationsScreenView(route: "dashboard"),
+                                    context);
+                                readMain.hideBottomNavigationBar();
+                              },
+                              child:
+                                  //  watch.notificationdata?.notificationStatus ==
+                                  //         "unseen"
+                                  //     ?
+                                  SvgPicture.asset(
+                                      "assets/icons/notification_shop.svg")
+                              // : Container(
+                              //   height: 50.h,
+                              //   width: 50.h,
+                              //   color: Black,
+                              // ),
+                              ),
                         ],
                       ),
                     ),
